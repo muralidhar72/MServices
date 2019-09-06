e120fc8081e1dedab2dc81e63ea0e5fc8ed383bc
p0089060 <p0089060@oxfordbrookes.ac.uk>
2019-09-06 17:51:50 +0100

    Code

---
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/LimitConfigurationProxy.java
@@ -0,0 +1,12 @@
+package com.in28minutes.microservices.currencyconversionservice;
+
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name="limits-service")
+public interface LimitConfigurationProxy {
+	@GetMapping("/limits")
+	public LimitConfiguration getLimits();
+}
