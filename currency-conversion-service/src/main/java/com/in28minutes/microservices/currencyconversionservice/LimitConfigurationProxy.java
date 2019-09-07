cdbb458e94502342494292ccd5217ed14671064d
Murali <murli.pl@gmail.com>
2019-09-07 10:57:58 +0100

    Changed to Dynamic Port

--- currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/LimitConfigurationProxy.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/LimitConfigurationProxy.java
@@ -5,8 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="limits-service")
+@FeignClient(name="netflix-zuul-api-gateway-server")
+@RibbonClient(name="limits-service")
 public interface LimitConfigurationProxy {
-	@GetMapping("/limits")
+	@GetMapping("/limits-service/limits")
 	public LimitConfiguration getLimits();
 }
